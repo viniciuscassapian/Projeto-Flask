268564361c2c4e876e5a0a77ed5b04ee0438cf7f
viniciuscassapian <vinicius.cassapian@aluno.faculdadeimpacta.com.br>
2025-05-05 19:56:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,101 +1,36 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flasgger import Swagger
+from base_de_dados import db
+from models.professor_model import Professor
+from models.turma_model import Turma
+from models.aluno_model import Aluno
+from controllers.professor_controller import professor_bp
+from controllers.turma_controller import turma_bp
+from controllers.aluno_controller import aluno_bp
+from config import DEBUG
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-professores = [
-    {"id": 1, "nome": "Prof. João", "idade": 40, "materia": "Matematica", "observacoes": "Experiente"},
-    {"id": 2, "nome": "Prof. Maria", "idade": 35, "materia": "Historia", "observacoes": "Experiente em história antiga"},
-]
+app.config['SWAGGER'] = {
+    'title': 'API de Gestão Escolar',
+    'uiversion': 3
+}
+swagger = Swagger(app)
 
-turmas = [
-    {"id": 1, "descricao": "Turma 1A", "professor_id": 1, "ativo": True},
-    {"id": 2, "descricao": "Turma 2B", "professor_id": 2, "ativo": True}
-]
+db.init_app(app)
 
-alunos = [
-    {"id": 1, "nome": "Ana", "idade": 14, "turma_id": 1, "data_nascimento": "2010-05-14", "nt_prim_sem": 8.5, "nt_seg_sem": 9.0, "media_final": 8.75},
-    {"id": 2, "nome": "Bruno", "idade": 15, "turma_id": 2, "data_nascimento": "2009-08-22", "nota_primeiro_semestre": 7.0, "nota_segundo_semestre": 6.5, "media_final": 6.75}
-]   
-# Essa parte é o CRUD dos professores
-# Esse GET serve para retornar a lista de professores
-@app.route('/professores', methods = ['GET'])
-def get_professores():
-    return jsonify(professores), 200
-# O POST vai adicionar um novo professor
-@app.route('/professores', methods=['POST'])
-def add_professor():
-    novo_professor = request.json
-    professores.append(novo_professor), 201
-# O PUT vai servir para atualizar um professor que já existe
-@app.route('/professores/<int:id>', methods =['PUT'])
-def update_professor(id):
-    for professor in professores:
-        if professor['id'] == id:
-            dados = request.json
-            professor.update(dados)
-        return jsonify(professor), 200
-    return jsonify({"erro": "Professor não encontrado"}), 404
-# O DELETE vai servir para deletar/remover um professor
-@app.route('/professores/<int:id>', methods = ['DELETE'])
-def deletar_professor(id):
-    for professor in professores:
-        if professor['id'] == id:
-            professores.remove(professor)
-            return jsonify({"mensagem": "Professor removido"}), 200
-        return jsonify({"erro": "Professor não encontrado"}), 404
-# Essa é a parte do CRUD das turmas
-# Esse é o GET das turmas
-@app.route('/turmas', methods = ['GET'])
-def get_turmas():
-    return jsonify(turmas), 200
-# O POST das turmas
-@app.route('/turmas', methods = ['POST'])
-def add_turma():
-    nova_turma = request.json
-    turmas.append(nova_turma)
-    return jsonify(nova_turma), 201
-# Esse é o PUT das turmas
-@app.route('/turmas/<int:id>', methods = ['PUT'])
-def update_turma(id):
-    for turma in turmas:
-        if turma ['id'] == id:
-            dados = request.json
-            turma.update(dados)
-            return jsonify(turma), 200
-        return jsonify({"erro": "Turma não encontrada"}), 404
-# Esse é o DELETE das turmas
-@app.route('/turmas/<int:id>', methods = ['DELETE'])
-def deletar_turmas(id):
-    for turma in turmas:
-        if turma ['id'] == id:
-            turmas.remove(turma)
-            return jsonify({"mensagem": "Turma removida"}), 200
-        return jsonify({"erro": "Turma não encontrada"}), 404
-# Esse é o CRUD dos alunos
-# #Esse é o GET dos alunos
-app.route('/alunos', methods = ['GET'])
-def get_alunos():
-    return jsonify(alunos), 200
-# Esse é o POST dos alunos
-app.route('/alunos', methods = ['POST'])
-def add_aluno():
-    novo_aluno = request.json
-    alunos.append(novo_aluno)
-    return jsonify(alunos), 201
-# Esse é PUT dos alunos
-app.route('/alunos/<int:id>', methods = ['PUT'])
-def update_aluno(id):
-    for aluno in alunos:
-        if aluno ['id'] == id:
-            dados = request.json
-            aluno.update(dados)
-            return jsonify(aluno), 200
-        return jsonify({"erro": "Aluno não encontrado"}), 404
-# Esse é o DELETE dos alunos
-app.route('/alunos/<int:id>', methods = ['DELETE'])
-def deletar_aluno(id):
-    for aluno in alunos:
-        if aluno['id'] == id:
-            alunos.remove(aluno)
-            return jsonify({'mensagem': "Aluno removido"}), 200
-        return jsonify({"erro": "Aluno não encontrado"})
+app.register_blueprint(professor_bp)
+app.register_blueprint(turma_bp)
+app.register_blueprint(aluno_bp)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return 'API Flask funcionando! Acesse /apidocs para ver a documentação Swagger.'
+
+if __name__ == "__main__":
+    app.run(debug=True,host= '0.0.0.0',port=5000)
