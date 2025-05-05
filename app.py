b4d892f8ea46060a832fa8e25a3bd86e26ba4d70
viniciuscassapian <vinicius.cassapian@aluno.faculdadeimpacta.com.br>
2025-05-05 20:50:13 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def index():
     return 'API Flask funcionando! Acesse /apidocs para ver a documentação Swagger.'
 
 if __name__ == "__main__":
-    app.run(debug=True,host= '0.0.0.0',port=5000)
+    app.run(debug=DEBUG)
