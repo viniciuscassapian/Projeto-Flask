3f37f891b1046f865fd8bcbf4c5ebc7c04b9436b
viniciuscassapian <vinicius.cassapian@aluno.faculdadeimpacta.com.br>
2025-05-30 20:17:11 -0300

    Delete base_de_dados.py

--- base_de_dados.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-
-db = SQLAlchemy()
-
-def init_db(app: Flask):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
